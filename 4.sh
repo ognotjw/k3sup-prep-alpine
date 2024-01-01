16ae98fc5b34255f782bfb10256d6f3f831113a7
ognotjw <143287710+ognotjw@users.noreply.github.com>
2024-01-01 13:32:43 -0500

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -6,4 +6,8 @@ echo "SSH stuff"
 sudo sed "s,PasswordAuthentication yes,PasswordAuthentication no,g" "/etc/ssh/sshd_config"
 echo "Done"
 
+echo "Sudo stuff"
+sudo sed -i "/alpine ALL=(ALL) NOPASSWD:ALL/d" /etc/sudoers
+echo "Done
+
 exit 0
