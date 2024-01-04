4b62e7dcb503af55bb4d53cb9ebe33d9c4675766
ognotjw <143287710+ognotjw@users.noreply.github.com>
2024-01-04 18:12:17 -0500

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -3,7 +3,7 @@
 echo "Script has started"
 
 echo "SSH stuff"
-sudo sed "s,PasswordAuthentication yes,PasswordAuthentication no,g" "/etc/ssh/sshd_config"
+sudo sed -i "s,PasswordAuthentication yes,PasswordAuthentication no,g" "/etc/ssh/sshd_config"
 echo "Done"
 
 exit 0
